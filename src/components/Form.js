8fa54c382d8d866397e3d53bd8f6cadf7c61d05d
Wanjiru Mburu <wanjirumarionne@gmail.com>
2024-02-04 12:25:11 +0300

    Completed Controlled Components Code Along

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Form() {
+/*function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
@@ -11,6 +11,48 @@ function Form() {
       <button type="submit">Submit</button>
     </form>
   );
+}*/
+
+/*function Form() {
+  const [newsletter, setNewsletter] = useState(false);
+
+  function handleNewsletterChange(event) {
+    // .checked, not .value!
+    setNewsletter(event.target.checked);
+  }
+
+  return (
+    <form>
+      <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
+      <input
+        type="checkbox"
+        id="newsletter"
+        onChange={handleNewsletterChange}
+        checked instead of value 
+        checked={newsletter}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}*/
+
+function Form({
+  firstName,
+  lastName,
+  handleFirstNameChange,
+  handleLastNameChange,
+}) {
+  return (
+    <form>
+      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <input type="text" onChange={handleLastNameChange} value={lastName} />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default Form;
+
+
+
+
